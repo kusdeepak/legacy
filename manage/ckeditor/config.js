bd556e7aafabbec36b774a5c2f58c9b7b31c5348
kusdeepak <kussoftware@gmail.com>
2017-01-05 12:47:20 +0530

    Add files via upload

---
+++ manage/ckeditor/config.js
@@ -0,0 +1,41 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	 //config.language = 'bg';
+	 config.uiColor = '#F3F3F3';
+	 config.filebrowserUploadUrl = '../ckupload.php';	 
+	 
+	// config.toolbar = 'toolbarLight';
+
+
+   /* config.toolbar_toolbarLight =
+    [
+        ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Scayt'],
+        ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+        ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar', 'Link','Unlink','Anchor', 'Maximize'] ,
+        '/',
+        ['Styles','Format','Font','FontSize', 'Bold','Italic','Strike','NumberedList','BulletedList','Outdent','Indent','Blockquote', 'TextColor','BGColor'],
+
+    ];
+	
+	config.toolbar_Fullx =
+   [
+      ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+      ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+      ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+      '/',
+      ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+      ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
+      ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+      ['Link','Unlink','Anchor'],
+      ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar'],
+      '/',
+      ['Styles','Format','Font','FontSize'],
+      ['TextColor','BGColor'],
+      ['Maximize', 'ShowBlocks']
+   ];*/
+};
